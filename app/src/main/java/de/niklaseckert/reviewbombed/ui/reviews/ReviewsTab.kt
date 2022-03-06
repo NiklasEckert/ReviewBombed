b4f303e54b84b9ebe9bfe48dc704b81ced57f852
niklaseckert <niklas.eckert.1807@gmail.com>
2022-03-06 14:08:39 +0100

    Created temporary ReviewsTab.

---
+++ app/src/main/java/de/niklaseckert/reviewbombed/ui/reviews/ReviewsTab.kt
@@ -0,0 +1,34 @@
+package de.niklaseckert.reviewbombed.ui.reviews
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ReviewsTab() {
+    val scaffoldState = rememberScaffoldState()
+    Scaffold(
+        scaffoldState = scaffoldState
+    ) {
+        Box(
+            modifier = Modifier.background(MaterialTheme.colors.background)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                Text(text = "Reviews")
+            }
+        }
+    }
+}
