106975b2a9f3aacd39ef5d30e02fe71c51833b0e
jako264 <jakob.friedsam@gmx.de>
2022-03-06 17:34:12 +0100

    Created use_case GetAllCurrentlyPlaying.

---
+++ app/src/main/java/de/niklaseckert/reviewbombed/feature_home/domain/use_case/GetAllCurrentlyPlaying.kt
@@ -0,0 +1,15 @@
+package de.niklaseckert.reviewbombed.feature_home.domain.use_case
+
+import de.niklaseckert.reviewbombed.core.domain.model.GameExcerpt
+import de.niklaseckert.reviewbombed.core.util.Resource
+import de.niklaseckert.reviewbombed.feature_home.domain.repository.HomeRepository
+import kotlinx.coroutines.flow.Flow
+
+class GetAllCurrentlyPlaying(
+    private val repository: HomeRepository
+) {
+
+    operator fun invoke(): Flow<Resource<List<GameExcerpt>>> {
+        return repository.getCurrentlyPlaying()
+    }
+}
