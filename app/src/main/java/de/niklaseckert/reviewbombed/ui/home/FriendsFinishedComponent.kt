52848c52a6c921d996829bd184cd650b6785f6cf
niklaseckert <niklas.eckert.1807@gmail.com>
2022-03-07 01:50:33 +0100

    Added CurrentlyPlayingComponent.kt, FriendsPlayingComponent.kt and FriendsFinishedComponent.kt.

---
+++ app/src/main/java/de/niklaseckert/reviewbombed/ui/home/FriendsFinishedComponent.kt
@@ -0,0 +1,31 @@
+package de.niklaseckert.reviewbombed.ui.home
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import de.niklaseckert.reviewbombed.R
+import de.niklaseckert.reviewbombed.feature_home.presentation.FriendsFinishedViewModel
+
+@Composable
+fun FriendsFinishedComponent() {
+    val friendsFinishedViewModel: FriendsFinishedViewModel = hiltViewModel()
+    val friendsFinishedState = friendsFinishedViewModel.state.value
+
+    Text(
+        text = stringResource(id = R.string.friends_finished_headline),
+        fontSize = 26.sp,
+        fontWeight = FontWeight.Bold,
+        modifier = Modifier.padding(start = 8.dp)
+    )
+    Spacer(modifier = Modifier.height(16.dp))
+    GameExcerptLazyRow(gameExcerptListState = friendsFinishedState)
+
+}
---
+++ app/src/main/java/de/niklaseckert/reviewbombed/ui/home/FriendsPlayingComponent.kt
@@ -0,0 +1,31 @@
+package de.niklaseckert.reviewbombed.ui.home
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import de.niklaseckert.reviewbombed.R
+import de.niklaseckert.reviewbombed.feature_home.presentation.FriendsPlayingViewModel
+
+@Composable
+fun FriendsPlayingComponent() {
+    val friendsPlayingViewModel: FriendsPlayingViewModel = hiltViewModel()
+    val friendsPlayingState = friendsPlayingViewModel.state.value
+
+    Text(
+        text = stringResource(id = R.string.friends_playing_headline),
+        fontSize = 26.sp,
+        fontWeight = FontWeight.Bold,
+        modifier = Modifier.padding(start = 8.dp)
+    )
+    Spacer(modifier = Modifier.height(16.dp))
+    GameExcerptLazyRow(gameExcerptListState = friendsPlayingState)
+
+}
