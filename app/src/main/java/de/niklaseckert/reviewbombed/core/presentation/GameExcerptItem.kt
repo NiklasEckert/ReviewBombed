ca39c1f612bbc217268b7ff88d5c21bbe367d5c4
niklaseckert <niklas.eckert.1807@gmail.com>
2022-03-07 01:51:47 +0100

    Modified GameExcerptItem to display an shadow.

--- app/src/main/java/de/niklaseckert/reviewbombed/core/presentation/GameExcerptItem.kt
+++ app/src/main/java/de/niklaseckert/reviewbombed/core/presentation/GameExcerptItem.kt
@@ -2,9 +2,11 @@ package de.niklaseckert.reviewbombed.core.presentation
 
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -15,7 +17,12 @@ fun GameExcerptItem(
     gameExcerpt: GameExcerpt,
     modifier: Modifier = Modifier
 ) {
-    Card(modifier = modifier.width(128.dp).height(180.dp)) {
+    Card(
+        modifier = modifier
+            .width(128.dp)
+            .height(180.dp)
+            .shadow(2.dp, RoundedCornerShape(8.dp), false)
+    ) {
         AsyncImage(
             model = gameExcerpt.coverUrl,
             contentDescription = null,
