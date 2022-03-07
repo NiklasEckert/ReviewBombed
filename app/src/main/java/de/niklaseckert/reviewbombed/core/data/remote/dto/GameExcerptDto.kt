a8cc61b278ac237417c563c3e002d552d43b31ff
niklaseckert <niklas.eckert.1807@gmail.com>
2022-03-07 01:42:39 +0100

    Modified GameExcerptDto to include GameExcerptScope.

--- app/src/main/java/de/niklaseckert/reviewbombed/core/data/remote/dto/GameExcerptDto.kt
+++ app/src/main/java/de/niklaseckert/reviewbombed/core/data/remote/dto/GameExcerptDto.kt
@@ -1,17 +1,19 @@
 package de.niklaseckert.reviewbombed.core.data.remote.dto
 
 import de.niklaseckert.reviewbombed.core.data.local.entity.GameExcerptEntity
+import de.niklaseckert.reviewbombed.core.data.local.entity.GameExcerptScope
 
 data class GameExcerptDto(
     val id: Long,
     val title: String,
     val coverUrl: String
 ) {
-    fun toGameExcerptEntity(): GameExcerptEntity {
+    fun toGameExcerptEntity(scope: GameExcerptScope): GameExcerptEntity {
         return GameExcerptEntity(
             id = id,
             title = title,
-            coverUrl = coverUrl
+            coverUrl = coverUrl,
+            scope = scope
         )
     }
 }
