6b73008acbfd64ac2b2bee5fc1d157d15f5d89a9
niklaseckert <niklas.eckert.1807@gmail.com>
2022-03-07 01:31:45 +0100

    Renamed AllCurrentlyPlayingState.kt to GameExcerptListState.kt.

--- app/src/main/java/de/niklaseckert/reviewbombed/feature_home/presentation/GameExcerptListState.kt
+++ app/src/main/java/de/niklaseckert/reviewbombed/feature_home/presentation/GameExcerptListState.kt
@@ -2,7 +2,7 @@ package de.niklaseckert.reviewbombed.feature_home.presentation
 
 import de.niklaseckert.reviewbombed.core.domain.model.GameExcerpt
 
-data class AllCurrentlyPlayingState(
+data class GameExcerptListState(
     val gameExcerptItems: List<GameExcerpt> = emptyList(),
     val isLoading: Boolean = false
 ) {
