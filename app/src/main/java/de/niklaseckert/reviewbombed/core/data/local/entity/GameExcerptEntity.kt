6ad58e4247e2dfa762486e672dfa842a31011782
niklaseckert <niklas.eckert.1807@gmail.com>
2022-03-07 01:38:39 +0100

    Modified GameExcerptEntity to include GameExcerptScope.

--- app/src/main/java/de/niklaseckert/reviewbombed/core/data/local/entity/GameExcerptEntity.kt
+++ app/src/main/java/de/niklaseckert/reviewbombed/core/data/local/entity/GameExcerptEntity.kt
@@ -4,17 +4,25 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import de.niklaseckert.reviewbombed.core.domain.model.GameExcerpt
 
-@Entity
+@Entity(primaryKeys = ["id", "scope"])
 data class GameExcerptEntity(
-    @PrimaryKey val id: Long? = null,
+    val id: Long,
     val title: String,
-    val coverUrl: String
+    val coverUrl: String,
+    val scope: GameExcerptScope
 ) {
     fun toGameExcerpt(): GameExcerpt {
         return GameExcerpt(
-            id = id ?: -1,
+            id = id,
             title = title,
             coverUrl = coverUrl
         )
     }
+}
+
+enum class GameExcerptScope {
+    NONE,
+    CURRENTLY_PLAYING,
+    FRIENDS_PLAYING,
+    FRIENDS_FINISHED
 }
