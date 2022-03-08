5ba4c1d953d15fe827ba6a4ceea86dfe5b56fdec
niklaseckert <niklas.eckert.1807@gmail.com>
2022-03-08 13:13:01 +0100

    Made GameExcerptItem clickable.

--- app/src/main/java/de/niklaseckert/reviewbombed/core/presentation/GameExcerptItem.kt
+++ app/src/main/java/de/niklaseckert/reviewbombed/core/presentation/GameExcerptItem.kt
@@ -1,5 +1,6 @@
 package de.niklaseckert.reviewbombed.core.presentation
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -9,24 +10,27 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import de.niklaseckert.reviewbombed.core.domain.model.GameExcerpt
 
 @Composable
 fun GameExcerptItem(
     gameExcerpt: GameExcerpt,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    navController: NavController
 ) {
     Card(
         modifier = modifier
             .width(128.dp)
             .height(180.dp)
             .shadow(2.dp, RoundedCornerShape(8.dp), false)
+            .clickable { navController.navigate("game/" + gameExcerpt.id) }
     ) {
         AsyncImage(
             model = gameExcerpt.coverUrl,
             contentDescription = null,
-            contentScale = ContentScale.Crop
+            contentScale = ContentScale.Crop,
         )
     }
 }
