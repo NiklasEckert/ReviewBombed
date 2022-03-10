1e8a187a8d6ccfc01faf517cdb85c9bfa8f1bb2c
niklaseckert <niklas.eckert.1807@gmail.com>
2022-03-10 20:58:12 +0100

    Created GeneralUnits to hold static values for padding and font-sizes.

---
+++ app/src/main/java/de/niklaseckert/reviewbombed/ui/theme/GeneralUnits.kt
@@ -0,0 +1,10 @@
+package de.niklaseckert.reviewbombed.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+class GeneralUnits() {
+    companion object {
+        val BASE_PADDING = 8.dp
+        val COMPONENT_SPACER_HEIGHT = 16.dp
+    }
+}
