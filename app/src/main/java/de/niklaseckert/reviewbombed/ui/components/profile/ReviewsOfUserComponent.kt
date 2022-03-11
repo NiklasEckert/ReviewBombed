dea96bce3e3513934ebf032388df3bbb72673063
jako264 <jakob.friedsam@gmx.de>
2022-03-11 17:41:24 +0100

    Created reviews of user component.

---
+++ app/src/main/java/de/niklaseckert/reviewbombed/ui/components/profile/ReviewsOfUserComponent.kt
@@ -0,0 +1,58 @@
+package de.niklaseckert.reviewbombed.ui.components.profile
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import de.niklaseckert.reviewbombed.R
+import de.niklaseckert.reviewbombed.feature_review.domain.model.Review
+import de.niklaseckert.reviewbombed.ui.components.items.ReviewExcerptItem
+import de.niklaseckert.reviewbombed.ui.theme.GeneralUnits
+
+@Composable
+fun ReviewsOfUserComponent(
+    navController: NavController,
+    reviews: List<Review>
+) {
+    val configuration = LocalConfiguration.current
+
+    Spacer(
+        modifier = Modifier
+            .height(GeneralUnits.COMPONENT_SPACER_HEIGHT)
+    )
+    Column() {
+        Text(
+            text = stringResource(id = R.string.review_headline),
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold
+        )
+        
+        Spacer(modifier = Modifier.height(GeneralUnits.COMPONENT_SPACER_HEIGHT))
+
+        LazyRow {
+            items(reviews.size) { index ->
+                Card() {
+                    ReviewExcerptItem(
+                        review = reviews[index],
+                        navController = navController,
+                        modifier = Modifier
+                            .width(configuration.screenWidthDp.dp - GeneralUnits.DOUBLE_SPACER_WIDTH)
+                    )
+                }
+                
+                Spacer(modifier = Modifier.width(GeneralUnits.BASE_PADDING))
+            }
+        }
+    }
+}
