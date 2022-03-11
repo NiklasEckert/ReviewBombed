4b260ecb72777588d64ec7d68cd0b45025a2d88a
jako264 <jakob.friedsam@gmx.de>
2022-03-11 17:34:32 +0100

    Added Card to review screen.

--- app/src/main/java/de/niklaseckert/reviewbombed/ui/screens/ReviewsScreen.kt
+++ app/src/main/java/de/niklaseckert/reviewbombed/ui/screens/ReviewsScreen.kt
@@ -2,23 +2,23 @@ package de.niklaseckert.reviewbombed.ui.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Card
 import androidx.compose.material.Divider
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import de.niklaseckert.reviewbombed.R
 import de.niklaseckert.reviewbombed.ui.components.items.ReviewExcerptItem
 import de.niklaseckert.reviewbombed.feature_review.presentation.ReviewsViewModel
 import de.niklaseckert.reviewbombed.ui.components.ReviewBombedCustomTopBar
+import de.niklaseckert.reviewbombed.ui.theme.GeneralUnits
 
 @Composable
 fun ReviewsScreen(
@@ -41,12 +41,24 @@ fun ReviewsScreen(
                 items(reviewsState.reviewItems.size) { index ->
                     val review = reviewsState.reviewItems[index]
 
-                    ReviewExcerptItem(review = review, navController = navController)
-
-                    Divider(
+                    Card(
                         modifier = Modifier
-                            .padding(horizontal = 8.dp)
-                    )
+                            .fillMaxWidth()
+                            .padding(GeneralUnits.BASE_PADDING)
+                    ) {
+                        ReviewExcerptItem(
+                            review = review,
+                            navController = navController,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    }
+
+                    if (index < reviewsState.reviewItems.size-1) {
+                        Divider(
+                            modifier = Modifier
+                                .padding(horizontal = 8.dp)
+                        )
+                    }
                 }
             }
         }
