d0f3750561b95417fb06e5f4c071985b99412ff0
jako264 <jakob.friedsam@gmx.de>
2022-03-11 17:41:32 +0100

    Created lists of user component.

---
+++ app/src/main/java/de/niklaseckert/reviewbombed/ui/components/profile/ListsOfUserComponent.kt
@@ -0,0 +1,47 @@
+package de.niklaseckert.reviewbombed.ui.components.profile
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import de.niklaseckert.reviewbombed.R
+import de.niklaseckert.reviewbombed.core.domain.model.ListExcerpt
+import de.niklaseckert.reviewbombed.ui.components.items.ListExcerptItem
+import de.niklaseckert.reviewbombed.ui.theme.GeneralUnits
+
+@Composable
+fun ListsOfUserComponent(
+    navController: NavController,
+    lists: List<ListExcerpt>
+) {
+
+    Spacer(
+        modifier = Modifier
+            .height(GeneralUnits.COMPONENT_SPACER_HEIGHT)
+    )
+
+    Column() {
+        Text(
+            text = stringResource(id = R.string.list_headline),
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Spacer(modifier = Modifier.height(GeneralUnits.COMPONENT_SPACER_HEIGHT))
+
+        Column {
+            lists.forEach { list ->
+                ListExcerptItem(
+                    listExcerpt = list,
+                    navController = navController
+                )
+            }
+        }
+    }
+}
