c03eb7e907654a94f55fd0021b6ebb85287fa15c
niklaseckert <niklas.eckert.1807@gmail.com>
2022-03-13 14:42:50 +0100

    Implemented BottomSheet to display AddReviewSheet.

---
+++ app/src/main/java/de/niklaseckert/reviewbombed/ui/components/gamedetail/AddReviewSheet.kt
@@ -0,0 +1,166 @@
+package de.niklaseckert.reviewbombed.ui.components.gamedetail
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import de.niklaseckert.reviewbombed.feature_game.domain.model.Game
+import de.niklaseckert.reviewbombed.ui.components.ReviewBombedRatingBar
+import de.niklaseckert.reviewbombed.ui.components.general.GameDetailHeadline
+import de.niklaseckert.reviewbombed.ui.components.items.GameExcerptItem
+import de.niklaseckert.reviewbombed.ui.theme.GeneralUnits
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun AddReviewSheet(
+    game: Game,
+    sheetState: ModalBottomSheetState
+) {
+
+    var reviewTitle by remember { mutableStateOf("") }
+    var reviewText by remember { mutableStateOf("") }
+
+    val configuration = LocalConfiguration.current
+
+    var sliderPosition by remember { mutableStateOf(0f) }
+
+    val scope = rememberCoroutineScope()
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(configuration.screenHeightDp.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Row(
+            modifier = Modifier
+                .padding(GeneralUnits.BASE_PADDING)
+                .fillMaxWidth()
+                .padding(GeneralUnits.BASE_PADDING),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Button(
+                onClick = {
+                    scope.launch {
+                        sheetState.hide()
+                        reviewText = ""
+                        reviewTitle = ""
+                        sliderPosition = 0f
+                    }
+                }
+            ) {
+                Text(text = "Cancel")
+            }
+
+            Text(
+                text = "I played ...",
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp
+            )
+
+            Button(
+                onClick = { /*TODO*/ }
+            ) {
+                Text(text = "Save")
+            }
+        }
+
+        Spacer(modifier = Modifier.height(GeneralUnits.COMPONENT_SPACER_HEIGHT))
+
+        Row(
+            modifier = Modifier
+                .background(MaterialTheme.colors.background)
+                .padding(
+                    horizontal = GeneralUnits.BASE_PADDING,
+                    vertical = GeneralUnits.BASE_PADDING + GeneralUnits.BASE_PADDING
+                )
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Card(
+                modifier = Modifier
+                    .width(90.dp)
+                    .height(127.dp)
+                    .shadow(2.dp, RoundedCornerShape(8.dp), false)
+            ) {
+                AsyncImage(
+                    model = game.coverUrl,
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                )
+            }
+            Spacer(modifier = Modifier.width(GeneralUnits.COMPONENT_SPACER_HEIGHT))
+            GameDetailHeadline(text = game.title)
+
+        }
+
+        Spacer(modifier = Modifier.height(GeneralUnits.COMPONENT_SPACER_HEIGHT))
+
+        Divider()
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(GeneralUnits.BASE_PADDING)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = "Rating")
+                ReviewBombedRatingBar(rating = (sliderPosition + 1).toInt())
+            }
+            
+            Slider(
+                value = sliderPosition,
+                onValueChange = {
+                    sliderPosition = it
+                },
+                valueRange = 0f..4f,
+                steps = 3
+            )
+        }
+        Divider()
+        Spacer(modifier = Modifier.height(GeneralUnits.COMPONENT_SPACER_HEIGHT))
+        
+
+        TextField(
+            value = reviewTitle,
+            onValueChange = { reviewTitle = it },
+            label = { Text(text = "Title") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(GeneralUnits.BASE_PADDING)
+        )
+
+        Spacer(modifier = Modifier.height(GeneralUnits.COMPONENT_SPACER_HEIGHT))
+
+        TextField(
+            value = reviewText,
+            onValueChange = { reviewText = it },
+            label = { Text(text = "Text") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(configuration.screenHeightDp.dp / 3)
+                .padding(GeneralUnits.BASE_PADDING),
+            maxLines = 10
+        )
+
+        Spacer(modifier = Modifier.height(GeneralUnits.COMPONENT_SPACER_HEIGHT))
+    }
+}
--- app/src/main/java/de/niklaseckert/reviewbombed/ui/screens/GameDetailScreen.kt
+++ app/src/main/java/de/niklaseckert/reviewbombed/ui/screens/GameDetailScreen.kt
@@ -3,7 +3,7 @@ package de.niklaseckert.reviewbombed.ui.screens
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalConfiguration
@@ -13,6 +13,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import de.niklaseckert.reviewbombed.feature_game.presentation.GameViewModel
 import de.niklaseckert.reviewbombed.ui.components.developerexcerpts.DeveloperExcerptListComponent
+import de.niklaseckert.reviewbombed.ui.components.gamedetail.AddReviewSheet
 import de.niklaseckert.reviewbombed.ui.components.gamedetail.GameDetailFloatingActionButton
 import de.niklaseckert.reviewbombed.ui.components.gamedetail.GameDetailPreviewImage
 import de.niklaseckert.reviewbombed.ui.components.general.GameDetailHeadline
@@ -20,8 +21,10 @@ import de.niklaseckert.reviewbombed.ui.components.general.LocalDateText
 import de.niklaseckert.reviewbombed.ui.components.publisherexcerpts.PublisherExcerptListComponent
 import de.niklaseckert.reviewbombed.ui.components.screenshots.ScreenshotExcerptListComponent
 import de.niklaseckert.reviewbombed.ui.theme.GeneralUnits
+import kotlinx.coroutines.launch
 import kotlin.math.min
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun GameDetailScreen(
     gameViewModel: GameViewModel = hiltViewModel(),
@@ -31,94 +34,117 @@ fun GameDetailScreen(
     val rememberScrollState = rememberScrollState()
     val configuration = LocalConfiguration.current
 
+    var skipHalfExpanded by remember { mutableStateOf(false) }
+    val scope = rememberCoroutineScope()
+
+    val sheetState = rememberModalBottomSheetState(
+        initialValue = ModalBottomSheetValue.Hidden,
+        skipHalfExpanded = skipHalfExpanded
+    )
+
     gameState.gameItem?.let { game ->
 
-        Scaffold(
-            topBar = {
-//                ReviewBombedCustomTopBar(text = game.title)
-            }
+        ModalBottomSheetLayout(
+            sheetState = sheetState,
+            sheetContent = {
+                AddReviewSheet(game, sheetState = sheetState)
+            },
+            sheetElevation = 26.dp
         ) {
-            ConstraintLayout(
-                modifier = Modifier
-                    .verticalScroll(rememberScrollState)
+            Scaffold(
+                topBar = {
+//                ReviewBombedCustomTopBar(text = game.title)
+                }
             ) {
-                val (image, sp, fab, info, comps) = createRefs()
-
-                GameDetailPreviewImage(
-                    url = game.previewImageUrl,
-                    imageModifier = Modifier
-                        .constrainAs(image) { top.linkTo(parent.top) }
-                        .height(configuration.screenHeightDp.dp / 2)
-                        .graphicsLayer {
-                            alpha = min(1f, 1 - (rememberScrollState.value / 1500f))
-                            translationY = +rememberScrollState.value * 0.7f
-                        },
-                    spacerModifier = Modifier
-                        .constrainAs(sp) {
-                            bottom.linkTo(image.bottom)
-                        }
-                )
-
-                Box(
+                ConstraintLayout(
                     modifier = Modifier
-                        .background(MaterialTheme.colors.background)
-                        .constrainAs(info) {
-                            top.linkTo(image.bottom)
-                        }
+                        .verticalScroll(rememberScrollState)
                 ) {
-                    Column(
-                        modifier = Modifier.padding(GeneralUnits.BASE_PADDING)
-                    ) {
-                        GameDetailHeadline(text = game.title)
-                        LocalDateText(date = game.date)
-                        Text(
-                            text = game.description
-                        )
-                    }
-                }
+                    val (image, sp, fab, info, comps) = createRefs()
 
-                Box(
-                    modifier = Modifier
-                        .background(MaterialTheme.colors.background)
-                        .constrainAs(comps) {
-                            top.linkTo(info.bottom)
+                    GameDetailPreviewImage(
+                        url = game.previewImageUrl,
+                        imageModifier = Modifier
+                            .constrainAs(image) { top.linkTo(parent.top) }
+                            .height(configuration.screenHeightDp.dp / 2)
+                            .graphicsLayer {
+                                alpha = min(1f, 1 - (rememberScrollState.value / 1500f))
+                                translationY = +rememberScrollState.value * 0.7f
+                            },
+                        spacerModifier = Modifier
+                            .constrainAs(sp) {
+                                bottom.linkTo(image.bottom)
+                            }
+                    )
+
+                    Box(
+                        modifier = Modifier
+                            .background(MaterialTheme.colors.background)
+                            .constrainAs(info) {
+                                top.linkTo(image.bottom)
+                            }
+                    ) {
+                        Column(
+                            modifier = Modifier.padding(GeneralUnits.BASE_PADDING)
+                        ) {
+                            GameDetailHeadline(text = game.title)
+                            LocalDateText(date = game.date)
+                            Text(
+                                text = game.description
+                            )
                         }
-                ) {
-                    Column(
-                        modifier = Modifier.padding(GeneralUnits.BASE_PADDING)
-                    ) {
-                        Spacer(modifier = Modifier.height(GeneralUnits.COMPONENT_SPACER_HEIGHT))
-                        Divider()
-                        ScreenshotExcerptListComponent(
-                            screenshots = game.screenshots,
-                            navController = navController
-                        )
-
-                        Spacer(modifier = Modifier.height(GeneralUnits.COMPONENT_SPACER_HEIGHT))
-                        Divider()
-                        PublisherExcerptListComponent(
-                            navController = navController,
-                            publishers = game.publishers
-                        )
-
-                        Spacer(modifier = Modifier.height(GeneralUnits.COMPONENT_SPACER_HEIGHT))
-                        DeveloperExcerptListComponent(
-                            navController = navController,
-                            developers = game.developers
-                        )
                     }
 
-                }
+                    Box(
+                        modifier = Modifier
+                            .background(MaterialTheme.colors.background)
+                            .constrainAs(comps) {
+                                top.linkTo(info.bottom)
+                            }
+                    ) {
+                        Column(
+                            modifier = Modifier.padding(GeneralUnits.BASE_PADDING)
+                        ) {
+                            Spacer(modifier = Modifier.height(GeneralUnits.COMPONENT_SPACER_HEIGHT))
+                            Divider()
+                            ScreenshotExcerptListComponent(
+                                screenshots = game.screenshots,
+                                navController = navController
+                            )
 
-                GameDetailFloatingActionButton(
-                    modifier = Modifier
-                        .constrainAs(fab) {
-                            centerAround(image.bottom)
-                            absoluteRight.linkTo(parent.absoluteRight, margin = 16.dp)
+                            Spacer(modifier = Modifier.height(GeneralUnits.COMPONENT_SPACER_HEIGHT))
+                            Divider()
+                            PublisherExcerptListComponent(
+                                navController = navController,
+                                publishers = game.publishers
+                            )
+
+                            Spacer(modifier = Modifier.height(GeneralUnits.COMPONENT_SPACER_HEIGHT))
+                            DeveloperExcerptListComponent(
+                                navController = navController,
+                                developers = game.developers
+                            )
                         }
-                )
+
+                    }
+
+                    GameDetailFloatingActionButton(
+                        modifier = Modifier
+                            .constrainAs(fab) {
+                                centerAround(image.bottom)
+                                absoluteRight.linkTo(parent.absoluteRight, margin = 16.dp)
+                            },
+                        onClick = {
+                            scope.launch {
+                                sheetState.animateTo(ModalBottomSheetValue.Expanded)
+                            }
+                        }
+                    )
+                }
             }
         }
+
+
 
     }
 }
