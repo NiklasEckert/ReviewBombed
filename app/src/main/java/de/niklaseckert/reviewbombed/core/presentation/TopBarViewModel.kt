ed5c3c1cd4808cf873bd67947c36fc7a5c00a570
jako264 <jakob.friedsam@gmx.de>
2022-03-14 16:30:01 +0100

    Created documentation for Top bar View Model package.

--- app/src/main/java/de/niklaseckert/reviewbombed/core/presentation/TopBarViewModel.kt
+++ app/src/main/java/de/niklaseckert/reviewbombed/core/presentation/TopBarViewModel.kt
@@ -7,10 +7,23 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 
+/**
+ * Class which represents the Top Bar.
+ *
+ * @author Niklas Eckert
+ * @author Jakob Friedsam
+ */
 class TopBarViewModel() : ViewModel() {
+
+    /** Represents the text that is displayed on the bar. */
     var topBarText by mutableStateOf("TEST")
+
+    /** Represents if the bar is enabled. */
     var isEnabled by mutableStateOf(true)
+
+    /** Represents the padding of the top bar. */
     var topBarPadding by mutableStateOf(0.dp)
 }
 
+/** Represents the Top Bar State. */
 val TopBarState = compositionLocalOf<TopBarViewModel> { error("TopBar State Context Not Found!") }
